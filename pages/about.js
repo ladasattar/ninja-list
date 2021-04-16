72c94da73921b824dc98db49859701c194d2ebf0
ladasattar <ladasattar@gmail.com>
2021-04-16 11:08:52 +0800

    init

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+const About = () => {
+	return (
+		<>
+			<Head>
+				<title>Ninja List | About</title>
+				<meta name="keywords" content="ninjas" />
+			</Head>
+			<div>
+				<h1>About</h1>
+				<p>Sint eiusmod consectetur duis eiusmod labore ea laboris occaecat in ut. Reprehenderit nisi Lorem velit ullamco aliqua sint amet sit minim aute mollit incididunt culpa. Aliqua Lorem anim ea sunt est esse. Proident laboris ut cupidatat aliqua amet. Nisi voluptate occaecat occaecat sunt sint esse aute. Ullamco consectetur laborum magna sunt duis esse et excepteur laboris nisi sint sit amet. Dolor ad voluptate sit labore id.</p>
+				<p>Sint eiusmod consectetur duis eiusmod labore ea laboris occaecat in ut. Reprehenderit nisi Lorem velit ullamco aliqua sint amet sit minim aute mollit incididunt culpa. Aliqua Lorem anim ea sunt est esse. Proident laboris ut cupidatat aliqua amet. Nisi voluptate occaecat occaecat sunt sint esse aute. Ullamco consectetur laborum magna sunt duis esse et excepteur laboris nisi sint sit amet. Dolor ad voluptate sit labore id.</p>
+			</div>
+		</>
+	);
+}
+
+export default About;
